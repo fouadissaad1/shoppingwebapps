5dee6021adc26a18b52db79bfd6347df3ce6e713
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-10-09 05:03:04 +0200

    install + import mdb test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import React from "react";
+import { MDBBtn } from "mdb-react-ui-kit";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-        <h1>welkom</h1>
-    </div>
+      // eslint-disable-next-line react/jsx-no-undef
+      <>
+          <MDBBtn color="primary">Primary</MDBBtn>
+          <MDBBtn>Default</MDBBtn>
+          <MDBBtn color="secondary">Secondary</MDBBtn>
+          <MDBBtn color="success">Success</MDBBtn>
+          <MDBBtn color="info">Info</MDBBtn>
+          <MDBBtn color="warning">Warning</MDBBtn>
+          <MDBBtn color="danger">Danger</MDBBtn>
+      </>
   );
 }
 
