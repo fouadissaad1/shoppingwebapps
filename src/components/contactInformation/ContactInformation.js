5ae0cd01e19c4b318c23e0a46d72177d10e43249
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-10-10 17:09:49 +0200

    page and compentens contact

---
+++ src/components/contactInformation/ContactInformation.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function ContactInformation() {
+    return <p>You can contact clothing in various ways. Below is an overview of the different contact
+        options.</p>
+}
+
+export function Consumer() {
+    return <p>Question?<br></br>
+        klantenservice@clothingshop.be <br></br>
+        +32 (0) 493 99 99 44 <br></br>
+    </p>
+}
+
+export function ServiceWarranty() {
+    return <p> service@clothingshop.be</p>
+}
+
+export function Payments() {
+    return <p>Question? <br/>
+        administratie@cothingshop.be</p>
+}
+
+export function Business() {
+    return <p>Question?<br/>
+        verkoop@cothingshop.be<br/>
+        +32 (0) 493 99 99 45</p>
+}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,55 @@
-import React from "react";
+import { MDBBtn, MDBCol, MDBCollapse, MDBRow } from "mdb-react-ui-kit";
+import React, { useState } from "react";
+import { Business, Consumer, ContactInformation, Payments, ServiceWarranty } from "../components/contactInformation/ContactInformation";
 export function Contact(){
+    const [showShow, setShowShow] = useState(false);
+    const toggleShow = () => setShowShow(!showShow);
     return(
         <>
-            <p>Contact</p>
+            <React.Fragment>
+                {/* eslint-disable-next-line react/jsx-no-undef */}
+                <MDBRow/>
+                <MDBRow color={"red"}>
+                    {/* eslint-disable-next-line react/jsx-no-undef */}
+                    <MDBCol lg={1}/>
+                    <MDBCol lg={2}>
+
+                        {/* eslint-disable-next-line react/jsx-no-undef,no-undef */}
+                        <MDBBtn onClick={toggleShow}>Contact information</MDBBtn>
+                        {/* eslint-disable-next-line react/jsx-no-undef */}
+                        <MDBCollapse show={showShow}>
+                            <ContactInformation></ContactInformation>
+                        </MDBCollapse>
+                    </MDBCol>
+                    <MDBCol lg={2}>
+                        <MDBBtn onClick={toggleShow}>Consumer</MDBBtn>
+                        <MDBCollapse show={showShow}>
+                            <Consumer/>
+                        </MDBCollapse>
+                    </MDBCol>
+                    <MDBCol>
+                        <MDBBtn onClick={toggleShow}>Service and warranty</MDBBtn>
+                        <MDBCollapse show={showShow}>
+                            <ServiceWarranty/>
+                        </MDBCollapse>
+                    </MDBCol>
+                    <MDBCol lg={2}>
+                        <MDBBtn onClick={toggleShow}>Payments and administrative</MDBBtn>
+                        <MDBCollapse show={showShow}>
+                            <Payments/>
+                        </MDBCollapse>
+                    </MDBCol>
+                    <MDBCol lg={2}>
+                        <MDBBtn onClick={toggleShow}>For Business</MDBBtn>
+                        <MDBCollapse show={showShow}>
+                            <Business/>
+                        </MDBCollapse>
+
+                    </MDBCol>
+                    <MDBCol lg={1}></MDBCol>
+                </MDBRow>
+                <MDBRow></MDBRow>
+            </React.Fragment>
         </>
     )
 }
