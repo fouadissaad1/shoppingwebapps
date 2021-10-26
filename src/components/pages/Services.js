c0f22274e8ee17aa5ef1b1d153d81efb67d818d9
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-10-26 07:07:26 +0200

    page serives

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -3,51 +3,63 @@ import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
 export function Services() {
     return (<MDBContainer fluid>
             <MDBRow>
-                <MDBCol size={4}/>
-                <MDBCol size={4}>
+                <MDBCol size={3}/>
+                <MDBCol size={6}>
                     <h3>Our Services</h3>
                     <h2>Services and experiences</h2>
                     <p>Expert advicce and inspiration, whenever you neet it</p>
-                    <p>We're pleased to offer the service you know and love both in sore and virtually, so you can expert advicie wheren you are and hower you choose</p>
+                    <p>We're pleased to offer the service you know and love both in sore and virtually, so you can
+                        expert advicie wheren you are and hower you choose</p>
                 </MDBCol>
-                <MDBCol size={4}/>
+                <MDBCol size={3}/>
             </MDBRow>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
-            <div>
-                <hr/>
-            </div>
-            <div>
-
-            </div>
-            <div>
-                <p>pict1</p>
-                <p>pic 2</p>
-                <p>pic3</p>
-            </div>
-            <div>
-                <div>
+            <MDBRow>
+                <MDBCol size={3}></MDBCol>
+                <MDBCol size={3}>
+                    <img
+                        src="https://mdbootstrap.com/img/new/standard/city/047.jpg"
+                        className="img-fluid rounded-pill"
+                        alt=""
+                    />
+                    <img
+                        src="https://mdbootstrap.com/img/new/standard/city/047.jpg"
+                        className="img-fluid rounded-pill"
+                        alt=""
+                    />
+                </MDBCol>
+                <MDBCol size={3}>
+                    <img
+                        src="https://mdbootstrap.com/img/new/standard/city/047.jpg"
+                        className="img-fluid rounded-pill"
+                        alt=""
+                    />
+                    <img
+                        src="https://mdbootstrap.com/img/new/standard/city/047.jpg"
+                        className="img-fluid rounded-pill"
+                        alt=""
+                    />
+                </MDBCol>
+                <MDBCol size={3}></MDBCol>
+            </MDBRow>
+            <hr/>
+            <MDBRow>
+                <MDBCol size={3}></MDBCol>
+                <MDBCol size={3}>
                     <h3>John Lewis Rentals Powered by fat LLALMA</h3>
                     <p>Rent our furniture, whenever youneed it, to create a look you love without the momitment</p>
                     <p>find out more Link</p>
-                </div>
-                <div>
-                    <p>picture</p>
-                </div>
-            </div>
+                </MDBCol>
+                <MDBCol size={3}>
+                    <img
+                        src="https://mdbootstrap.com/img/new/standard/city/047.jpg"
+                        class="img-fluid rounded"
+                        alt=""
+                    />
+                </MDBCol>
+                <MDBCol size={3}></MDBCol>
+            </MDBRow>
             <hr/>
-            <div>
-                <p>Asselbkt service</p>
-                <p> clik op + annd more info</p>
-                <p>Baby and child services</p>
-                <p> clik op + annd more info</p>
-
-            </div>
-    </MDBContainer>
+        </MDBContainer>
 
     )
 }
