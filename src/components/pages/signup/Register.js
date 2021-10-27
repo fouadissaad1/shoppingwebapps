a6098155f8e4bf822e89bc910b19473dfa53af09
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-10-27 22:58:24 +0200

    sign up

--- src/components/pages/signup/Register.js
+++ src/components/pages/signup/Register.js
@@ -1,13 +1,27 @@
 import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
 import { auth } from "firebase-admin/lib/auth/auth-namespace";
 import firebaseConfig from "../../../services/firestore";
+import { useState } from "react";
 
 function Register() {
-    firebaseConfig.auth()
-        .createUserWithEmailAndPassword('test@yahoo.fr','ccccccccc')
-        .then(()=>{
-            console.log('user account created')
-        })
+    const [currentUser, setCurrentUser] = useState(null);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { email, password } = e.target.elements;
+        try {
+            firebaseConfig.auth().createUserWithEmailAndPassword(email.value,password.value)
+                .then(()=>setCurrentUser(true))
+        }catch (error){
+            if (error.code === 'auth/email-already-in-use') {
+                alert('That email address is already in use!')
+
+            }
+
+            if (error.code === 'auth/invalid-email') {
+                alert('That email address is invalid!')
+            }
+        }
+    };
     return (
         <MDBContainer>
             <br/><br/>
@@ -15,14 +29,14 @@ function Register() {
                 <MDBCol size={4}/>
                 <MDBCol size={4}>
                     <h1>Sign up</h1>
-                    <form >
+                    <form onSubmit={handleSubmit} >
                         <label for="email">
                             email <br/>
                             <input name="email" type="email" placeholder={"email"} required/>
                         </label>
                         <label for="password">
                             Password<br/>
-                            <input name="Password" type={"Password"} placeholder={"Password"} />
+                            <input name="password" type={"Password"} placeholder={"Password"} />
                             <br/><br/>
                             <button type={"submit"}>Sign Up</button>
                         </label>
