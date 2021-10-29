58dcd90daf1a8266605b9d58b9745780e7b4912a
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-10-29 16:18:48 +0200

    home page read foto from datastore

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import { MDBCol, MDBContainer, MDBIcon, MDBRow } from "mdbreact";
 import React from "react";
+
 function Home() {
     return (
         <>
@@ -21,19 +22,27 @@ function Home() {
                 </MDBRow>
                 <MDBRow>
                     <MDBCol lg="3" md="12">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/shopping-ef663.appspot.com/o/images%2Fwelcome%2Fdiy-cat-costumes-1565110063.jpg?alt=media&token=b2ab3cda-10ae-4064-98f4-272ebb856397" className="img-fluid z-depth-1" alt=""/>
+                        <img
+                            src="https://firebasestorage.googleapis.com/v0/b/shopping-ef663.appspot.com/o/images%2Fwelcome%2Fdiy-cat-costumes-1565110063.jpg?alt=media&token=b2ab3cda-10ae-4064-98f4-272ebb856397"
+                            className="img-fluid z-depth-1" alt=""/>
                     </MDBCol>
                     <MDBCol lg="3" md="12">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/shopping-ef663.appspot.com/o/images%2Fwelcome%2Fabdf6295852cb3fa42bb5b55556848a6.jpg?alt=media&token=eec82a32-f0f2-4e53-aac4-e25fa8a01c2c" className="img-fluid z-depth-1-half"
-                             alt=""/>
+                        <img
+                            src="https://firebasestorage.googleapis.com/v0/b/shopping-ef663.appspot.com/o/images%2Fwelcome%2Fabdf6295852cb3fa42bb5b55556848a6.jpg?alt=media&token=eec82a32-f0f2-4e53-aac4-e25fa8a01c2c"
+                            className="img-fluid z-depth-1-half"
+                            alt=""/>
                     </MDBCol>
                     <MDBCol lg="3" md="12">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/shopping-ef663.appspot.com/o/images%2Fwelcome%2FJenniferKellyG-201635210035178721None.jpg?alt=media&token=b2fba25c-720c-42db-94b6-6be39719e301" className="img-fluid z-depth-1-half"
-                             alt=""/>
+                        <img
+                            src="https://firebasestorage.googleapis.com/v0/b/shopping-ef663.appspot.com/o/images%2Fwelcome%2FJenniferKellyG-201635210035178721None.jpg?alt=media&token=b2fba25c-720c-42db-94b6-6be39719e301"
+                            className="img-fluid z-depth-1-half"
+                            alt=""/>
                     </MDBCol>
                     <MDBCol lg="3" md="12">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/shopping-ef663.appspot.com/o/images%2Fwelcome%2Fkids1.jpg?alt=media&token=77a5f3e9-c597-4632-bfcf-e5dac771e99d" className="img-fluid z-depth-1-half"
-                             alt=""/>
+                        <img
+                            src="https://firebasestorage.googleapis.com/v0/b/shopping-ef663.appspot.com/o/images%2Fwelcome%2Fkids1.jpg?alt=media&token=77a5f3e9-c597-4632-bfcf-e5dac771e99d"
+                            className="img-fluid z-depth-1-half"
+                            alt=""/>
                     </MDBCol>
                     <MDBRow>
                     </MDBRow>
