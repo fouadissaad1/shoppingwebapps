37cac7eeda636695e1184082c5f97a474d76a3e1
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-11-01 18:56:45 +0100

    DELIVERY

--- src/components/pages/Delivery.js
+++ src/components/pages/Delivery.js
@@ -1,4 +1,4 @@
-import { MDBBtn, MDBCol, MDBCollapse, MDBContainer, MDBIcon, MDBRow } from "mdb-react-ui-kit";
+import { MDBCol, MDBCollapse, MDBContainer, MDBIcon, MDBRow } from "mdb-react-ui-kit";
 import { useState } from "react";
 
 
@@ -27,19 +27,21 @@ export function Delivery() {
                             ></MDBIcon>
                         </h3>
                         <MDBCollapse show={showBelivered}>
-                            <p>In most cases your whole order will be delivered at the same time. The following exceptions
+                            <p>In most cases your whole order will be delivered at the same time. The following
+                                exceptions
                                 apply:</p>
                             <h3 className="red-text">LargeItems</h3>
-                            <p>Delayed items will be despatched as soon as stock becomes available. You can track the progress
+                            <p>Delayed items will be despatched as soon as stock becomes available. You can track the
+                                progress
                                 of
                                 your order by going to My Account and selecting ‘Order Tracking’ on the left side, under
                                 "Account
                                 Options".</p>
                         </MDBCollapse>
                     </div>
-<br/>
+                    <br/>
                     <div className="text-center md-4">
-                        <h3  className="cyan-text">When i receve delayed items?
+                        <h3 className="cyan-text">When i receve delayed items?
                             <MDBIcon
                                 icon='caret-down'
                                 className='cyan-text'
@@ -50,10 +52,11 @@ export function Delivery() {
                         </h3>
 
                         <MDBCollapse show={showReceve}>
-                            <p>When your item becomes available for despatch, we will email you so you can track your order</p>
+                            <p>When your item becomes available for despatch, we will email you so you can track your
+                                order</p>
                         </MDBCollapse>
                     </div>
-<br/>
+                    <br/>
                     <div className="text-center md-4">
                         <h3 className="cyan-text">Can I have order delivered to an alternative or wordk adress?
                             <MDBIcon
