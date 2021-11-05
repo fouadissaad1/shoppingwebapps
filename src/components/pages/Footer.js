f6d9408f5ba367c783a209a4cbce8a66aeb0ebc1
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-11-05 19:01:29 +0100

    reformat code home

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -9,35 +9,35 @@ function Footer() {
             <div className="footer-copyright text-center py-3">
                 <MDBContainer fluid>
                     <MDBRow>
-                        <MDBCol md={9}>
+                        <MDBCol md="9">
                             <MDBRow>
-                                <MDBCol md={3}>
+                                <MDBCol md="3">
                                     <Link to="/Book">Book an Appointement</Link>
                                 </MDBCol>
-                                <MDBCol md={3}>
+                                <MDBCol md="3">
                                     <Link to="/Services">Exclusive Online Services</Link>
                                 </MDBCol>
-                                <MDBCol md={1}>
+                                <MDBCol md="1">
                                     <Link to="/Delivery">Delivery</Link>
                                 </MDBCol>
-                                <MDBCol md={1}>
+                                <MDBCol md="1">
                                     <Link to="/Contact">Contact </Link>
                                 </MDBCol>
-                                <MDBCol md={1}>
+                                <MDBCol md="1">
                                     <Link to="/About"> About</Link>
                                 </MDBCol>
-                                <MDBCol md={1}>
+                                <MDBCol md="1">
                                     <Link to="/Men">Men </Link>
                                 </MDBCol>
-                                <MDBCol md={1}>
+                                <MDBCol md="1">
                                     <Link to="/Women"> Women </Link>
                                 </MDBCol>
-                                <MDBCol md={1}>
+                                <MDBCol md="1">
                                     <Link to="/Children"> Children </Link>
                                 </MDBCol>
                             </MDBRow>
                         </MDBCol>
-                        <MDBCol md={3}>
+                        <MDBCol md="3">
                             <p> &copy; {new Date().getFullYear()} Belgium Country Clothing • Fouad ISSAAD</p>
                         </MDBCol>
                     </MDBRow>
