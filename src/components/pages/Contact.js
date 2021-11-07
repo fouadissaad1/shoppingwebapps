bd362a43eb403b0198a3acfa0674cc6a7fd5da3b
fouadissaad1 <71667992+fouadissaad1@users.noreply.github.com>
2021-11-07 08:40:50 +0100

    reformat code contact + change size to md in MDBCOL

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -23,58 +23,56 @@ export function Contact() {
     return (
         <>
             <React.Fragment>
-                <br/>
+                <br/> <br/> <br/> <br/>
                 <MDBRow>
-                    <MDBCol size="4"></MDBCol>
-                    <MDBCol size="4">
+                    <MDBCol md="4"/>
+                    <MDBCol md="4">
                         <MDBBtn onClick={togglesInformation}>Contact information</MDBBtn>
                         <MDBCollapse show={showShowInformation}>
                             <ContactInformation/>
                         </MDBCollapse>
                     </MDBCol>
-                    <MDBCol size="4"/>
+                    <MDBCol md="4"/>
                 </MDBRow>
                 <MDBRow>
-                    <MDBCol size="4"></MDBCol>
-                    <MDBCol size="4">
+                    <MDBCol md="4"/>
+                    <MDBCol md="4">
                         <MDBBtn onClick={togglesConsumer}>Consumer</MDBBtn>
                         <MDBCollapse show={showConsumer}>
                             <Consumer/>
                         </MDBCollapse>
                     </MDBCol>
-                    <MDBCol size="4">
-
-                    </MDBCol>
+                    <MDBCol md="4"/>
                 </MDBRow>
                 <MDBRow>
-                    <MDBCol size="4"></MDBCol>
-                    <MDBCol size="4">
+                    <MDBCol md="4"/>
+                    <MDBCol md="4">
                         <MDBBtn onClick={togglesServices}>Service and warranty</MDBBtn>
                         <MDBCollapse show={showServices}>
                             <ServiceWarranty/>
                         </MDBCollapse>
                     </MDBCol>
-                    <MDBCol size="4"></MDBCol>
+                    <MDBCol md="4"/>
                 </MDBRow>
                 <MDBRow>
-                    <MDBCol size="4"></MDBCol>
-                    <MDBCol size="4">
+                    <MDBCol md="4"/>
+                    <MDBCol md="4">
                         <MDBBtn onClick={togglesPayments}>Payments and administrative</MDBBtn>
                         <MDBCollapse show={showPayments}>
                             <Payments/>
                         </MDBCollapse>
                     </MDBCol>
-                    <MDBCol size="4"></MDBCol>
+                    <MDBCol md="4"/>
                 </MDBRow>
                 <MDBRow>
-                    <MDBCol size="4"></MDBCol>
-                    <MDBCol size="4">
+                    <MDBCol md="4"/>
+                    <MDBCol md="4">
                         <MDBBtn onClick={toggleBusiness}>For Business</MDBBtn>
                         <MDBCollapse show={showBusiness}>
                             <Business/>
                         </MDBCollapse>
                     </MDBCol>
-                    <MDBCol size="4"></MDBCol>
+                    <MDBCol md="4"/>
                 </MDBRow>
             </React.Fragment>
         </>
